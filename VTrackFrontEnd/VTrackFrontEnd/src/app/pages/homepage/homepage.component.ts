e0ce78b502a147619a465d43f3990fd61690ea78
Chocorins2 <88347537+Chocorins2@users.noreply.github.com>
2022-07-25 03:44:34 +0700

    create homepage component (temporarily)

---
+++ VTrackFrontEnd/VTrackFrontEnd/src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {SubSink} from 'subsink';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+})
+export class HomepageComponent implements OnInit, OnDestroy {
+  private subSink: SubSink;
+
+  constructor(public dialog: MatDialog, private router: Router) {
+    this.subSink = new SubSink();
+  }
+
+  public ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+    this.subSink.unsubscribe();
+  }
+}
