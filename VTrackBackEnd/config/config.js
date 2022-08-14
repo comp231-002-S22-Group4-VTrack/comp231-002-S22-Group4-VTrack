415d0e998bc930e1a9ac5ee8f968cb6e154a0163
Chocorins2 <nho17@my.centennialcollege.ca>
2022-08-14 09:15:21 -0400

    fix config

--- VTrackBackEnd/config/config.js
+++ VTrackBackEnd/config/config.js
@@ -1,2 +1,3 @@
 // Load the correct configuration file according to the 'NODE_ENV' variable
-module.exports = require("./env/" + 'development' + ".js");
+module.exports = require("./env/" + process.env.NODE_ENV + ".js");
+
