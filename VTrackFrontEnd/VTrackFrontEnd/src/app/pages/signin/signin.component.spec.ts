d1f54f6d7f9477a151607f3dcd237132cc1b7526
Chocorins2 <nho17@my.centennialcollege.ca>
2022-08-13 21:02:11 -0400

    add more tests

---
+++ VTrackFrontEnd/VTrackFrontEnd/src/app/pages/signin/signin.component.spec.ts
@@ -0,0 +1,66 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PatientService } from 'src/app/services/patient/patient.service';
+import { SigninComponent } from './signin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from '../../app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialAngularModule } from '../../material-angular.module';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+
+describe('SigninComponent', () => {
+  let component: SigninComponent;
+  let fixture: ComponentFixture<SigninComponent>;
+  // tslint:disable-next-line:prefer-const
+  let patientService: any;
+  const formBuilder: FormBuilder = new FormBuilder();
+  const mockPatientService = {
+    signIn: () => of(null)
+  };
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [SigninComponent],
+      imports: [
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialAngularModule,
+        SharedModule,
+        RouterModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilder },
+        { provide: PatientService, useValue: mockPatientService }
+      ],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SigninComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    patientService = jasmine.createSpyObj(['signIn']);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('can sign in', () => {
+    component.signInForm.controls.email.setValue('localvac@yopmail.com');
+    component.signInForm.controls.password.setValue('Tester123!');
+    component.submitSignIn();
+    // tslint:disable:no-string-literal
+    spyOn(component['patientService'], 'signIn');
+  });
+});
